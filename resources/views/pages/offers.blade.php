31fbe2fa2e0ab71ffa82c48856389f441a4bf939
MM Williams <mmwilliamscodes@gmail.com>
2020-03-12 15:16:05 -0400

    added all static pages now working on menu page

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -1,7 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="">
-     <h1>Offers Page</h1>
+    <div id="offers-page">
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <h1>Sign Up To Receive Notifications for Deals</h1>
+                    <form>
+                        <div class="row">
+                            <div class="col-md-6">
+                                 <div class="form-group">
+                                    <label for="firstNameInput">First Name</label>
+                                    <input type="fname" class="form-control" name="text" id="firstNameInput" placeholder="John">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="lastNameInput">Last Name</label>
+                                    <input type="text" class="form-control" name="lname" id="lastNameInput" placeholder="Doe">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                 <div class="form-group">
+                                    <label for="emailInput">Email Address</label>
+                                    <input type="email" class="form-control" name="email" id="emailInput" placeholder="name@example.com">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="phoneInput">Phone Number</label>
+                                    <input type="text" class="form-control" name="phone" id="phoneInput" placeholder="555-555-5555">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <p>In signing up I acknowledge that I am 18 years of age or older, want to receive email offers from Billy's Burgers and, if I select to join Dine Rewards, agree to the terms and conditions of the program.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary mb-2">Confirm</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/pages/waitlist.blade.php
+++ resources/views/pages/waitlist.blade.php
@@ -1,7 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="">
-     <h1>Waitlist Page</h1>
+    <div id="waitlist-page">
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Get On The List</h1>
+                    <form>
+                        <div class="form-group">
+                            <label for="emailInput">Email Address</label>
+                            <input type="email" class="form-control" name="email" id="emailInput" placeholder="name@example.com">
+                        </div>
+                        <div class="form-group">
+                            <label for="phoneInput">Phone Number</label>
+                            <input type="text" class="form-control" name="phone" id="phoneInput" placeholder="555-555-5555">
+                        </div>
+                        <div class="form-group">
+                            <label for="guestInput">How Many Guests</label>
+                            <select class="form-control" name="guests" id="guestInput">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="timeInput">What Time</label>
+                            <select class="form-control" name="time" id="timeInput">
+                            <option value="6">6:00 PM</option>
+                            <option value="7">7:00 PM</option>
+                            <option value="8">8:00 PM</option>
+                            <option value="9">9:00 PM</option>
+                            <option value="10">10:00 PM</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary mb-2">Confirm</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <p>Please Note: This is not a reservation. You will be added to the current wait list. You may have a short wait once you arrive while we prepare your table.</p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
